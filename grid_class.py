2c5bf6008af6c54f5c2b0da9f391103b6302fe77
Iken Omar <59393587+oiken98@users.noreply.github.com>
2020-11-07 11:42:25 +0100

    Update grid_class.py

--- grid_class.py
+++ grid_class.py
@@ -1,10 +1,12 @@
-# ## Liberaires
+## Liberaires
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# ## The Grid Class
+
+## The Grid Class
+
 
 class Grid(object):
     
@@ -14,25 +16,28 @@ class Grid(object):
     n_rects : <integer> n_rects**2 is the number of cells in the grid
     """
     
-    def __init__(self, x_coord:np.array=None, y_coord:np.array=None, n_rects:int=200):
+    def __init__(self, x_coord:np.array=None, y_coord:np.array=None, n_cells:int=200):
         self.x_coord = x_coord ## The x coordinates of trajectories
         self.y_coord = y_coord ## The y coordinates of trajectories
-        self.n_rects = n_rects ## the number of rectangles in the grid
+        self.n_cells = n_cells ## the number of rectangles in the grid
         
         ## getting the edge points and rectangle width and height of the grid
         self.get_edges_points()
         
         ## initialize the grid with 0 values 
-        self.init_grid()
+        self.initiate_grid()
         
         ## getting the rectangle for the trajectories
-        self.get_rects()
+        self.get_input_trajectories_cells()
         
-    def init_grid(self):    
+        
+    def initiate_grid(self):    
         
         """return a zero numpy array with the shape (n_rect x n_rects)"""
         
-        self.grid = np.zeros((self.n_rects + 1, self.n_rects + 1))
+        ## value 0 means that the cell is empty and 1 means that it's occupied
+        self.grid = np.zeros((self.n_cells + 1, self.n_cells + 1))
+        
     
     def get_edges_points(self):
         
@@ -43,111 +48,141 @@ class Grid(object):
         self.x1, self.y1 = max(self.x_coord)+1, max(self.y_coord)+1
         
         ## the rectangles width and height
-        self.rect_width  = (self.x1 - self.x0) / (self.n_rects - 1)
-        self.rect_height = (self.y1 - self.y0) / (self.n_rects - 1) 
+        self.cell_width  = (self.x1 - self.x0) / (self.n_cells - 1)
+        self.cell_height = (self.y1 - self.y0) / (self.n_cells - 1) 
     
-    def get_rects(self):
+    
+    def get_input_trajectories_cells(self):
         
         """return the projection of the x and y coordinates 
-        of the trajectory on the center of the rectangles of the grid"""
+        of the input trajectories on the center of the cells of the grid"""
         
         for x, y in zip(self.x_coord, self.y_coord):
-            i, j = self.get_rect(x, y)
+            
+            ## get the cell that corresponds to the coordiantes x and y
+            i, j = self.get_cell(x, y)
+            
+            ## swaping the value of the cell (i, j) to 1
             self.grid[i, j] = 1
+            
     
-    def get_rect(self, x:float, y:float) -> tuple:
+    def get_cell(self, x:float, y:float) -> tuple:
         
-        """return the rectangle in which the coordinates x and y are in"""
+        """return the cell in which the coordinates x and y are in"""
         
-        i = (x - self.x0) // self.rect_width + 1
-        j = (y - self.y0) // self.rect_height + 1
+        ## getting the cell in wich the coordinate (x, y) is in.
+        i = (x - self.x0) // self.cell_width  + 1
+        j = (y - self.y0) // self.cell_height + 1
         return int(i), int(j)
     
-    def get_rect_coord(self, i:int, j:int) -> tuple:
+    
+    def get_cell_coordinates(self, i:int, j:int) -> tuple:
         
-        """return the coordinates of the rectangle (i, j)"""
+        """return the coordinates of the cell (i, j)"""
         
-        x, y = self.rect_width * i, self.rect_height * j
-        x = self.x0 + self.rect_width  * (x // self.rect_width)
-        y = self.y0 + self.rect_height * (y // self.rect_height)
+        x, y = self.cell_width * i, self.cell_height * j
+        x = self.x0 + self.cell_width  * (x // self.cell_width)
+        y = self.y0 + self.cell_height * (y // self.cell_height)
         return x, y         
         
-    def get_rect_center(self, i:int, j:int) -> tuple: 
         
-        """return the cordinates of the center of the rectangle (i, j)"""
+    def get_cell_center(self, i:int, j:int) -> tuple: 
         
-        a, b = self.get_rect_coord(i, j)
-        return a + self.rect_width / 2 , b + self.rect_height / 2
+        """return the coordinates of the center of the cell (i, j)"""
+        
+        a, b = self.get_cell_coordinates(i, j)
+        return a + self.cell_width / 2 , b + self.cell_height / 2
 
-    def get_neighbours(self, i:int, j:int) -> list:
+    
+    def get_cell_neighbours(self, i:int, j:int) -> list:
         
-        """return the list of neighbours of the rectangle (i, j)"""
+        """return the list of neighbours of the cell (i, j)"""
         
         for v in range(max(0, i-4), min(i+5, len(self.grid))):
             for u in range(max(0, j-4), min(j+5, len(self.grid[0]))):
                 if self.grid[(v, u)] and any([i!=v, j!=u]):
                     return v, u
 
-    def get_trajectories_rects(self) -> list:
+                
+    def get_trajectories_cells(self) -> list:
         
-        """return the clustered trajectories"""
+        """return the cells of clustered trajectories"""
         
-        trajs = []
+        trajectories = []
         for i in range(len(self.grid)):
             for j in range(len(self.grid[0])):
                 if self.grid[(i, j)]:
                     a, b = i, j
-                    traj = [[a, b]]
+                    trajectory = [[a, b]]
                     self.grid[(a, b)] = 0
-                    while all([self.get_neighbours(a,b)]) :
-                        traj.append([a, b])
-                        a,b = self.get_neighbours(a, b)
+                    while all([self.get_cell_neighbours(a,b)]) :
+                        trajectory.append([a, b])
+                        a,b = self.get_cell_neighbours(a, b)
                         self.grid[(a, b)] = 0
-                    if len(traj) > 5:
-                        trajs.append(traj)
-        return np.array(trajs)
+                    trajectories.append(trajectory)
+        return np.array(trajectories)
+    
         
-    def get_trajectories_coord(self, trajs:np.array)->np.array:
+    def get_trajectories_coordinates(self, trajectories:np.array)->np.array:
         
-        """draw the path of each trajectory"""
+        """return the coordinates of clustered trajectories"""
         
-        trajs_coord = []
-        for traj in trajs:
+        trajectories_coordinates = []
+        for trajectory in trajectories:
             x_coord, y_coord = [], []
-            for rect in traj:
-                x0, y0 = self.get_rect_center(*rect)
-                x_coord.append(x0)
-                y_coord.append(y0)
-            trajs_coord.append([x_coord, y_coord])
-        return np.array(trajs_coord) 
+            for cell in trajectory:
+                x, y = self.get_cell_center(*cell)
+                x_coord.append(x)
+                y_coord.append(y)
+            trajectories_coordinates.append([x_coord, y_coord])
+        return np.array(trajectories_coordinates) 
+    
+    
+    def save_clustered_trajectories(self, file_name:str):
+        
+        """save the clustered trajectories with the given file name"""
+        
+        ## creating x coordinates and y coordinates
+        x_coordinates = []
+        y_coordinates = []
+        for trajectory in self.clustered_trajectories:
+            for x, y in zip(trajectory[0], trajectory[1]):
+                x_coordinates.append(x)
+                y_coordinates.append(y)
             
-    def plot_clustered_trajs(self, trajs_centers:np.array, ax):
+        ## create the data frame    
+        coordinates_dict = {'x' : x_coordinates, 'y' : y_coordinates}
+        df = pd.DataFrame.from_dict(coordinates_dict)
+        df.to_csv(f'{file_name}.csv')
+    
+            
+    def plot_clustered_trajectories(self, trajectories_centers:np.array):
         
         """plot the clustered trajectories"""
         
-        trajs_coord = self.get_trajectories_coord(trajs_centers)
-        for e, traj in enumerate(trajs_coord):
-            ax.scatter([traj[0][0], traj[0][-1]], [traj[-1][0], traj[1][-1]])
-            ax.plot(traj[0], traj[1], label='trajectory ' + str(e))
+        self.clustered_trajectories = self.get_trajectories_coordinates(trajectories_centers)
+        for e, trajectory in enumerate(self.clustered_trajectories):
+            plt.scatter([trajectory[0][0], trajectory[0][-1]], [trajectory[-1][0], trajectory[1][-1]])
+            plt.plot(trajectory[0], trajectory[1], label=f'trajectory {e}')
         
 
     def plot(self):
         
-        """draw the input and the output trajectories"""
-        plt.style.use('seaborn')
-        plt.rcParams["figure.figsize"] = 8, 8
-
-        fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharex=True)
-
-        ## ploting the input trajectories
-        ax1.plot(self.x_coord, self.y_coord)
-        ax1.set_title('Input trajectories')
+        """draw the clustered trajectories"""
         
-        ## ploting the clustered trajectories
-        trajs = self.get_trajectories_rects()
-        self.plot_clustered_trajs(trajs, ax2)
-        ax2.set_title(f'Clustered Trajectories for {self.n_rects}x{self.n_rects} cells')
-        ax2.set_xlabel('x')
-        ax2.set_ylabel('y')
-        ax2.legend(bbox_to_anchor=(1.25, 1))
+        ## using the seaborn style
+        plt.style.use('seaborn')
+        
+        ## resizing the plot
+        plt.rcParams["figure.figsize"] = 8, 8
+        
+        ## getting the trajectories cells
+        trajectories = self.get_trajectories_cells()
+        
+        ## plotting the clustered trajectories
+        self.plot_clustered_trajectories(trajectories)
+        plt.title('Clustered Trajectories')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.legend(bbox_to_anchor=(1.2, 1))
         plt.show()
